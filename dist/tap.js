7568878f6fc714c52afce17bc691979efedfa734
Liam Martens <hi@liammartens.com>
2015-06-27 16:47:18 +0200

    New version

--- dist/tap.js
+++ dist/tap.js
@@ -1,23 +1,47 @@
 (function() {
-	HTMLElement.prototype.tap = function(fn) {
-		var f = function() {
-			var dt = new Date().getTime();
-			if(event.type == "touchstart") {
-				this.isTouchStart = true;
-				this.lastTouchStart = dt;
-			} else if((this.isTouchStart==true)&&(dt - this.lastTouchStart < 30)&&(dt - this.lastTouchStart > 0)) {
-				this.isTouchStart = false;
+	HTMLElement.prototype.tap = function(fn, e_d, t_i) {
+		var func = function(e) {
+			e = e || window.event;
+			var callee = arguments.callee;
+			var now = new Date().getTime();
+			var event_type = e.type.toLocaleLowerCase().replace("on", "");
+			//check event type
+			if((event_type=="click")&&(now - callee.last_execute > callee.execute_delay)) {
+				callee.last_call = now;
+				callee.last_execute = now;
+				callee.last_event = "click";
 				fn.call(this);
+			} else if(event_type=="touchstart") {
+				callee.last_call = now;
+				callee.last_event = "touchstart";
+			} else if((event_type=="touchend")&&(callee.last_event=="touchstart")&&(now - callee.last_call < callee.tap_interval)&&(now - callee.last_execute > callee.execute_delay)) {
+				callee.last_call = now;
+				callee.last_execute = now;
+				callee.last_event = "touchend";
+				fn.call(this);
+			} else if(event_type=="touchmove") {
+				callee.last_call = now;
+				callee.last_event = "touchmove";
 			}
-		}
+		};
+		//set last call and last execute variables
+		func.last_call = new Date().getTime();
+		func.last_execute = new Date(1970, 1, 1);
+		func.last_event = null;
+		//standard execute delay
+		func.execute_delay = e_d || 60;
+		func.tap_interval = t_i || 60;
+		//attach event handlers
 		if(this.addEventListener) {
-			this.addEventListener("touchstart", f);
-			this.addEventListener("touchend", f);
-		} else {
-			this.attachEvent("ontouchstart", f);
-			this.attachEvent("ontouchend", f);
+			this.addEventListener("click", func);
+			this.addEventListener("touchstart", func);
+			this.addEventListener("touchend", func);
+			this.addEventListener("touchmove", func);
+		} else { 
+			this.attachEvent("onclick", func);
+			this.attachEvent("ontouchstart", func);
+			this.attachEvent("ontouchend", func);
+			this.attachEvent("ontouchmove", func);
 		}
-		this.isTouchStart = false;
-		this.lastTouchStart = new Date().getTime();
-	}
+	};
 })();
